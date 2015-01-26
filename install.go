db16ee6100d03e94fd9949790a4aeb306217a3d4
Hiroaki Nakakmura <hnakamur@gmail.com>
2015-01-26 02:04:39 -0800

    Change type of Install() from *InstallationResult to InstallationResult

--- install.go
+++ install.go
@@ -26,59 +26,60 @@ type UpdateResult struct {
 	ResultCode     int
 }
 
-func (s *Session) Install(updates []Update) (*InstallationResult, error) {
+func (s *Session) Install(updates []Update) (InstallationResult, error) {
+	empty := InstallationResult{}
 	installer, err := toIDispatchErr(oleutil.CallMethod((*ole.IDispatch)(s), "CreateUpdateInstaller"))
 	if err != nil {
-		return nil, err
+		return empty, err
 	}
 
 	coll, err := toUpdateCollection(updates)
 	if err != nil {
-		return nil, err
+		return empty, err
 	}
 	_, err = oleutil.PutProperty(installer, "Updates", coll)
 	if err != nil {
-		return nil, err
+		return empty, err
 	}
 
 	resultDisp, err := toIDispatchErr(oleutil.CallMethod(installer, "Install"))
 	if err != nil {
-		return nil, err
+		return empty, err
 	}
 
 	return toInstallationResult(resultDisp, len(updates))
 }
 
-func toInstallationResult(resultDisp *ole.IDispatch, updateCount int) (*InstallationResult, error) {
+func toInstallationResult(resultDisp *ole.IDispatch, updateCount int) (InstallationResult, error) {
 	result := InstallationResult{disp: resultDisp}
 	rebootRequired, err := toBoolErr(oleutil.GetProperty(resultDisp, "RebootRequired"))
 	if err != nil {
-		return nil, err
+		return result, err
 	}
 	result.RebootRequired = rebootRequired
 
 	resultCode, err := toInt64Err(oleutil.GetProperty(resultDisp, "ResultCode"))
 	if err != nil {
-		return nil, err
+		return result, err
 	}
 	result.ResultCode = int(resultCode)
 
 	for i := 0; i < updateCount; i++ {
 		urDisp, err := toIDispatchErr(oleutil.CallMethod(resultDisp, "GetUpdateResult", i))
 		if err != nil {
-			return nil, err
+			return result, err
 		}
 		rebootRequired, err := toBoolErr(oleutil.GetProperty(urDisp, "RebootRequired"))
 		if err != nil {
-			return nil, err
+			return result, err
 		}
 
 		resultCode, err := toInt64Err(oleutil.GetProperty(urDisp, "ResultCode"))
 		if err != nil {
-			return nil, err
+			return result, err
 		}
 		result.UpdateResults = append(result.UpdateResults,
 			UpdateResult{ResultCode: int(resultCode), RebootRequired: rebootRequired})
 	}
-	return &result, nil
+	return result, nil
 }
